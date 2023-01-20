637276f8cecbe54215c09e3ba451282df4077b51
Rohan <rohankumarchaudhary67@gmail.com>
2023-01-21 00:47:10 +0530

    Disabled the Next button if No further news to show

--- Second_Project_(NewsApp)/src/components/News.js
+++ Second_Project_(NewsApp)/src/components/News.js
@@ -57,7 +57,7 @@ export default class News extends Component {
                 </div>
                 <div className="container d-flex justify-content-between">
                     <button disabled={this.state.page<=1} type='button' className='btn btn-primary' onClick={this.handlePrevClick}>&larr; Previous</button>
-                    <button type='button' className='btn btn-primary' onClick={this.handleNextClick}>Next &rarr;</button>
+                    <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/20)} type='button' className='btn btn-primary' onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
