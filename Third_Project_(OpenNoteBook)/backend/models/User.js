2868652002bc8f619c498b34a62f2abca95b75c9
Rohan <rohankumarchaudhary67@gmail.com>
2023-01-21 21:58:10 +0530

    Creating Routes and Schemas for storing notes and user Data

---
+++ Third_Project_(OpenNoteBook)/backend/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("user", userSchema);
