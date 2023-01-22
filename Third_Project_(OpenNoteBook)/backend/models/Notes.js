b7e38e864bdace1de75a6cb93d6fb1e1634dee0a
Rohan <rohankumarchaudhary67@gmail.com>
2023-01-23 03:55:35 +0530

    Fetch all the notes of user and add a new note of a user features

--- Third_Project_(OpenNoteBook)/backend/models/Notes.js
+++ Third_Project_(OpenNoteBook)/backend/models/Notes.js
@@ -8,6 +8,13 @@ const {Schema} = mongoose;
 
 // Here create schema(It is structure of data which is send to db)
 const NotesSchema = new Schema({
+    // user is used to connect with the user (Because every user has own notes to handle)
+    user:{
+        // In this section we put the user id as a foreign key
+        type: mongoose.Schema.Types.ObjectId,
+        // ref is refrence (it means from which schema this key is obtained)
+        ref: 'user'
+    },
     title:{
         type: String,
         required: true  // It ensure that the element is null or not
--- Third_Project_(OpenNoteBook)/backend/routes/notes.js
+++ Third_Project_(OpenNoteBook)/backend/routes/notes.js
@@ -7,19 +7,101 @@ const express = require('express');
 // import the model or schema for structure of data
 const Notes = require('../models/Notes');
 
+// import express-validator
+const { body, validationResult } = require('express-validator');
+
+//import fetchuser from middleware to fetch the users data
+const fetchuser = require('../middleware/fetchuser');
+
 // create router from express to route
 const router = express.Router();
 
-// It is post request to send the data into database
-router.post('/', (req, res)=>{
-    const user = Notes({
-        title:"Title 2",
-        description:"lorem ipsum lorem sipsd sdijfdn sdiihh",
-        tag:"news"
-    });
-    user.save();
-    res.send("OK");
+
+
+
+
+
+
+
+
+
+// ##################################### ROUTE 1 ########################################
+
+// Fetch all the notes of user : GET "/api/auth/fetchallnotes" :  login required
+router.get('/fetchallnotes', fetchuser, async(req, res) => {
+
+    try {
+
+        // Fetch notes from db with the help of user id
+        const notes = await Notes.find({user: req.user.id});
+
+        // Response of this route
+        res.json(notes);
+        
+    } catch(error){
+        // If any error occured this message is shown
+        console.log(error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+
 })
+
+
+
+
+// ##################################### ROUTE 2 ########################################
+
+// Add the notes of user : POST "/api/auth/addnote" :  login required
+router.post('/addnote', fetchuser, [
+
+    // Here we use validation
+    body('title',"Title is too short | Minimum is 3").isLength({min: 3}),
+    // description must be at least 5 chars long
+    body('description', "Description is too short | Take minimum 5 character").isLength({min: 5})
+
+] ,async(req, res) => {
+
+    try {
+
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // destructuring the data from the body
+        const {title, description, tag} = req.body;
+
+        // Save the data of notes in a variable
+        const note = new Notes({
+            user: req.user.id,
+            title,
+            description,
+            tag
+        })
+
+        // Saved the notes in db
+        const savedNote = await note.save();
+
+        // Response of this route
+        res.json(savedNote);
+        
+    } catch(error){
+        // If any error occured this message is shown
+        console.log(error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+
+})
+
+
+
+
+
+
+
+
+
 
 // It export the router
 module.exports = router;
