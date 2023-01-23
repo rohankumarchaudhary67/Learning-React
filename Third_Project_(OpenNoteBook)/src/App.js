fd3cd70515a7230b6cc41b59b7dbaddcf08cb2fe
Rohan <rohankumarchaudhary67@gmail.com>
2023-01-23 17:01:17 +0530

    Creating Routes and Navbar for the React application

--- Third_Project_(OpenNoteBook)/src/App.js
+++ Third_Project_(OpenNoteBook)/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+// import the css file of app function or file
 import './App.css';
 
+// import the Navbar component
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Home from './components/Home';
+
+// import react router component from react-router-dom
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+
+
+// This is main function of the applicaton
 function App() {
+
+  // this app return this
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route exact path="/about" element={<About />}/>
+          </Routes>
+        </Router>
     </div>
   );
 }
 
+
+
+// export this app
 export default App;
